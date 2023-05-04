bd460baf28868c07fb07d29d88d1f720513d5d5b
kimdeokyoung <dudejrss@naver.com>
2023-05-04 16:42:42 +0900

    feat : 최대증가부분수열

--- c++/cote/GJM/최대증가부분수열.cpp
+++ c++/cote/GJM/최대증가부분수열.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int cache[N+1];
+int cache[N+1], answer[N+1];
 
 
 vector<int>& initialize(default_random_engine& gen){
@@ -36,6 +36,7 @@ int solve(vector<int>& v){
 				a.push_back(v[j]);
 			}
 		}
+
 		ret = max(ret, 1+solve(a));
 	}
 
@@ -61,18 +62,50 @@ int solve2(int cur, vector<int>& v){
 	return ret;
 }
 
+int solve2WithAnswer(int cur,vector<int> &v){
+	int& ret = cache[cur+1];
+
+	if(ret != -1) return ret;
+	ret = 1;
+	int best = -1;
+
+	for(int j=cur+1; j<v.size();j++){
+		if(cur == -1 || v[cur] < v[j]){
+			int tmp = solve2WithAnswer(j,v)+1;
+			if(ret < tmp){
+				ret = tmp;
+				best = j;
+			}
+		}
+	}
+
+	answer[cur+1] = best;
+	return ret;
+}
+
+void makeSeq(int cur, vector<int>& seq, vector<int>& v ){
+	if(cur != -1) seq.push_back(v[cur]);
+	int next = answer[cur+1];
+	if( next != -1) makeSeq(next, seq, v);
+}
+
 int main(void){
 
 	default_random_engine gen(43);
 
 	for (int i=0; i< TEST_CASES; i++){
 		vector<int>v = initialize(gen);
+		vector<int> seq;
 		memset(cache, -1, sizeof(cache));
 		cout << solve(v) << " ";
 		
 		memset(cache, -1, sizeof(cache));
-		cout << solve2(-1,v)-1 << endl;
-
+		cout << solve2WithAnswer(-1,v)-1 << endl;
+		makeSeq(-1,seq,v);
+		for(auto s : seq){
+			cout << s<< " ";
+		}
+		cout<< endl<< endl;
 	}
 
 	return 0;
