4da84011e7671f0d694ca3da424822e98d3045fc
kimdeokyoung <dudejrss@naver.com>
2023-05-04 15:47:13 +0900

    chore : make java workspace

---
+++ java/src/main/java/com/example/Test.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
