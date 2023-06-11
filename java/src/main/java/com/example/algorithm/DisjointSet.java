870fed958fc29669757863a6ffaf5831233880d0
kimdeokyoung <dudejrss@naver.com>
2023-06-11 22:30:06 +0900

    feat : DisjointSet

---
+++ java/src/main/java/com/example/algorithm/DisjointSet.java
@@ -0,0 +1,6 @@
+package com.example.algorithm;
+
+public interface DisjointSet {
+	int find(int a);
+	boolean union(int a, int b);
+};
---
+++ java/src/main/java/com/example/algorithm/LinkedListDisjointSet.java
@@ -0,0 +1,69 @@
+package com.example.algorithm;
+
+import java.util.*;
+import com.example.algorithm.*;
+import java.util.stream.*;
+
+public class LinkedListDisjointSet implements DisjointSet{
+
+	private static class Node {
+		int val;
+		Node representitive;
+		Node next;
+
+		Node(int val){
+			this.val = val;
+			this.representitive = this;
+			this.next = null;
+		}
+	};
+
+	private LinkedList<Node> groups;
+
+	LinkedListDisjointSet(int n) {
+		groups = new LinkedList<Node>();
+
+		IntStream.range(0,n)
+				.mapToObj(Node::new)
+				 .forEach(groups::add);
+	}
+
+	private void _chage_group(int rootA, int rootB){
+		if( rootA == rootB) return;
+		if(rootA != find(rootA) || rootB != find(rootB)) return;
+
+		Node cur = groups.get(rootA);
+
+		while(cur.next != null){
+			cur = cur.next;
+		}
+		cur.next = groups.get(rootB);
+		cur = groups.get(rootB);
+		while(cur != null){
+			cur.representitive = groups.get(rootA);
+			cur = cur.next;
+		}
+
+	}
+
+	public int find(int a){
+		Node target = groups.get(a).representitive;
+		return groups.indexOf(target);
+	}
+
+
+	public boolean union(int a, int b){
+		int rootA = find(a);
+		int rootB = find(b);
+		if( a == b ) return false;
+
+		if( a < b ) _chage_group(rootA, rootB);
+		else _chage_group(rootB, rootA);
+
+		return true;
+	}
+
+	public static void main(String... args){
+	}
+
+}
