29f8823c5acc1eed8016cfea3e0f9cb24f78a732
kimdeokyoung <dudejrss@naver.com>
2023-06-28 13:56:32 +0900

    feat : Bridge

--- c++/design_pattern/Bridge.cpp
+++ c++/design_pattern/Bridge.cpp
@@ -18,6 +18,7 @@ namespace Pimpl {
 		Person(string name);
 		~Person();
 
+		// 전방선언으로 구현부 선언과 그에 대한 포인터를 가진다
 		class PersonImpl;
 		PersonImpl* impl;
 	};
@@ -33,6 +34,7 @@ namespace Pimpl {
 	Person::Person(string name) : impl(new PersonImpl) {
 		this->name = name;
 	}
+	Person::~Person(){delete impl;}
 
 	void Person::greet(){
 		impl->greet(this);
@@ -51,24 +53,58 @@ namespace Pimpl {
 
 namespace Bridge{
 
-	struct Circle {
+	
+	struct Renderer {
+		virtual void render_circle(float x, float y, float radius){};
+	};
+
+	struct VectorRenderer : Renderer {
+		void render_circle(float x, float y, float radius)override{
+			cout << "Drawing a vector circle of radius " << radius << " at ("<<x <<","<<"y"<<")" << endl;
+		}
+	};
+
+	struct RasterRenderer : Renderer {
+		void render_circle(float x, float y, float radius)override{
+			cout << "Rastering circle of radius " << radius << " at ("<<x <<","<<"y"<<")" << endl;
+		}
+	};
+
+	struct Shape {
+		protected : 
+			Renderer& renderer;
+			Shape(Renderer& renderer) : renderer(renderer){}
+
+		public :
+			virtual void draw()=0;
+			virtual void resize(float factor) =0;
+	};
+
+	struct Circle : Shape {
+		
 		float x,y,radius;
 
-		void draw() {
+		void draw() override{
+			renderer.render_circle(x,y,radius);
+		}
+		void resize(float factor) override {
+			radius *= factor;
+		}
+
+		Circle(Renderer renderer, float x, float y, float radius) 
+		: Shape(renderer), x(x), y(y), radius(radius){
 
 		}
-		void resize(float factor){
 
-		}
-
-		Circle(float x, float y, float radius) : x(x), y(y), radius(radius){
-		}
 	};
 
 	void main(){
 
-		Circle raster_circle(5,5,5);
-		Circle vector_circle(5,5,5);
+		RasterRenderer rr;
+		VectorRenderer vr;
+
+		Circle raster_circle(rr, 5,5,5);
+		Circle vector_circle(vr, 5,5,5);
 
 		raster_circle.draw();
 		vector_circle.draw();
