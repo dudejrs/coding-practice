654d03e14dd476ae8624ec8095bea648db0bf277
kimdeokyoung <dudejrss@naver.com>
2023-06-28 13:36:47 +0900

    feat : Bridge

---
+++ c++/design_pattern/Bridge.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+namespace Pimpl {
+	using namespace std;
+
+	/* header */
+
+	// 다른 모듈이 include 하는 header에는 다음과 같은 필요한 public 인터페이스만 적재
+	struct Person {
+		string name;
+		void greet();
+		
+		Person(string name);
+		~Person();
+
+		class PersonImpl;
+		PersonImpl* impl;
+	};
+	
+
+
+	/* implementation */
+
+	struct Person::PersonImpl{
+
+	};
+
+	Person::Person(string name) {
+
+	}
+
+	void Person::greet(){
+
+	}
+
+	void main() {
+		// Person p1("Tom");
+		// p1.greet();
+	}
+
+};
+
+namespace Bridge{
+
+	struct Circle {
+		float x,y,radius;
+
+		void draw() {
+
+		}
+		void resize(float factor){
+
+		}
+
+		Circle(float x, float y, float radius) : x(x), y(y), radius(radius){
+		}
+	};
+
+	void main(){
+
+		Circle raster_circle(5,5,5);
+		Circle vector_circle(5,5,5);
+
+		raster_circle.draw();
+		vector_circle.draw();
+	}
+};
+
+int main(void){
+
+	Pimpl::main();
+	Bridge::main();
+
+	return 0;
+}
