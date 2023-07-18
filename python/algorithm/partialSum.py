4f6c73969b6d9f55bd9f4b26b599ecdb29f9a600
kimdeokyoung <dudejrss@naver.com>
2023-07-18 20:34:15 +0900

    feat : partial sum

--- python/algorithm/partialSum.py
+++ python/algorithm/partialSum.py
@@ -14,12 +14,16 @@ def partial_sum(t) :
 
 def range_sum(t, a, b) :
 	pSum = partial_sum(t)
+	if a == 0 : return pSum[b]
 	return pSum[b]- pSum[a-1];
+
 if __name__ == "__main__" :
 	n = 10
 	for _ in range(TEST_CASES) :
 		t = [ random.randrange(1,10) for i in range(n)]
-		(a, b) = random.smaple(range(n))
+		(a, b) = random.sample(range(n),2)
+		if a > b : a,b = b,a 
+		print(a,b)
 		print(range_sum(t, a, b))
 
 
