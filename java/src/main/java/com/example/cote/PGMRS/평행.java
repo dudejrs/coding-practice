27f9ebd601f88bb8c0359d5c8a808a27017feac9
kimdeokyoung <dudejrss@naver.com>
2023-09-21 18:18:00 +0900

    feat : 평행

---
+++ java/src/main/java/com/example/cote/PGMRS/평행.java
@@ -0,0 +1,57 @@
+package com.example.cote.PGMRS;
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class 평행 {
+
+	private static final int MAX_DOTS_NUMBER = 100;
+	private static final int DOTS_LENGTH = 4;
+
+	private static double getSlope(int[] a, int[] b){
+		return (double) (a[1] - b[1]) / (a[0]- b[0]);
+	}
+
+	private static final int solve(int[][] dots){
+
+		Set<Double> slopes = new HashSet<>();
+
+		for(int i=0; i<DOTS_LENGTH; i++){
+			for(int j=i+1; j<DOTS_LENGTH; j++){
+				double slope = getSlope(dots[i], dots[j]);
+
+				if(slopes.contains(slope)) return 1;
+				slopes.add(slope);
+			}
+		}
+
+		return 0;
+	}
+
+	public static void main(String... args) throws IOException {
+
+		Path p = Paths.get(System.getProperty("user.dir")+"/data/평행.txt");
+		BufferedReader fd = Files.newBufferedReader(p);
+
+		int testCases = Integer.parseInt(fd.readLine());
+
+		while(testCases > 0) {
+
+			int[][] dots = new int[DOTS_LENGTH][2];
+
+			for(int i =0; i<DOTS_LENGTH; i++){
+				dots[i] = Arrays.stream(fd.readLine().split(" "))
+							.mapToInt(Integer::parseInt)
+							.toArray();
+			}
+
+			System.out.println(solve(dots));
+
+			testCases--;
+		}
+
+
+		return ;
+	}
+}
