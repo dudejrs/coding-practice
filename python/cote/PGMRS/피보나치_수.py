0be46105dbd57707b3678f7319b91cf88328e65b
kimdeokyoung <dudejrss@naver.com>
2023-09-30 19:17:47 +0900

    feat : 피보나치 수

---
+++ python/cote/PGMRS/피보나치_수.py
@@ -0,0 +1,26 @@
+import random
+from functools import lru_cache
+
+
+TEST_CASES = 10;
+MAX_N = 100000;
+M = 1234567
+
+@lru_cache(100000)
+def fibonacci(n):
+	if n == 0 or n == 1 : return n
+	return (fibonacci(n-1) + fibonacci(n-2)) % M
+
+def solve(n) :
+
+	for i in range(n) :
+		fibonacci(i)
+
+	return fibonacci(n) 
+
+if __name__ == "__main__" :
+	random.seed(43);
+
+	for _ in range(TEST_CASES) :
+		n = random.randrange(1,MAX_N)
+		print(solve(n))
