4b8ce5b6950a1a8dd1480f7d1723d5bbf6183157
kimdeokyoung <dudejrss@naver.com>
2023-11-19 14:36:46 +0900

    feat: 팀 짜기

---
+++ java/src/main/java/com/example/cote/SMSNG/팀_짜기.java
@@ -0,0 +1,54 @@
+package com.example.cote.SMSNG;
+
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class 팀_짜기{
+
+	
+	private static int[] getNumbers(String s) {
+		return Arrays.stream(s.split(" "))
+		.mapToInt(Integer::parseInt)
+		.toArray();
+	}
+	
+	
+	
+	private static int getMaxNumberOfMembers(int target, int k, int[] numbers) {
+		return (int) Arrays.stream(numbers)
+				.filter(number -> number >= target && number <= target +k )
+				.count();
+	}
+	
+	
+	public static void main(String args[]) throws Exception{
+		Path p = Paths.get(System.getProperty("user.dir")+"/data/팀_짜기.txt");
+		BufferedReader rd = Files.newBufferedReader(p);
+		
+		Scanner sc = new Scanner(rd);
+		
+		int T =  Integer.parseInt(sc.nextLine());
+		
+		for(int test_case=1; test_case<=T; test_case++) {
+
+			int[] numbers = getNumbers(sc.nextLine());
+			int N = numbers[0];
+			int K = numbers[1];
+			
+			int[] levels = getNumbers(sc.nextLine());
+			Arrays.sort(levels);
+			
+			int max = 0;
+			
+			for(int number : levels) {
+				int count = getMaxNumberOfMembers(number, K, levels);
+				max = Math.max(max, count);
+			}
+			
+			System.out.println(max);
+		}
+	}
+}
+
