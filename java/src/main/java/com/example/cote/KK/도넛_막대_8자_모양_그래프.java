5eb93c710a976b83218587fdf028bf9ac624e15c
kimdeokyoung <dudejrss@naver.com>
2023-11-26 02:27:31 +0900

    feat: 도넛 막대 8자 모양 그래프

---
+++ java/src/main/java/com/example/cote/KK/도넛_막대_8자_모양_그래프.java
@@ -0,0 +1,40 @@
+package com.example.cote.KK;
+
+import java.util.*;
+import java.nio.file.*;
+import java.io.*;
+
+public class 도넛_막대_8자_모양_그래프 {
+
+	private static int[] solve(int[][] edges){
+
+		return new int[] {};
+	}
+	
+	public static void main(String... args) throws IOException{
+		Path p = Paths.get(System.getProperty("user.dir")+"/data/도넛_막대_8자_모양_그래프.txt");
+		BufferedReader rd = Files.newBufferedReader(p);
+
+		int testCases = Integer.parseInt(rd.readLine());
+
+		while(testCases > 0){
+			int edgeNum = Integer.parseInt(rd.readLine());
+
+			int[][] edges = new int [edgeNum][2];
+			for(int i=0; i<edgeNum; i++){
+				edges[i] = Arrays.stream(rd.readLine().split(" "))
+								.mapToInt(Integer::parseInt)
+								.toArray();
+			}
+
+			int[] answer = Arrays.stream(rd.readLine().split(" "))
+								.mapToInt(Integer::parseInt)
+								.toArray();
+
+			System.out.println("[Solve] :" + Arrays.toString(solve(edges)));
+			System.out.println("[Answer] :" + Arrays.toString(answer));
+
+			testCases--;
+		}
+	}
+}
