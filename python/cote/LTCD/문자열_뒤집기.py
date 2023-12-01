af84f1c894fb1ba91a6af4700e6b24204284b398
kimdeokyoung <dudejrss@naver.com>
2023-12-01 15:24:37 +0900

    feat: 문자열 뒤집기

---
+++ python/cote/LTCD/문자열_뒤집기.py
@@ -0,0 +1,20 @@
+from typing import *
+import random
+
+TEST_CASE = 10
+MAX_LENGTH = 10
+
+def solve(word : List[str]) -> None :
+	left, right = 0, len(word) -1;
+	while(left < right) :
+		word[left], word[right] = word[left], word[right]
+		left += 1
+		right -= 1
+
+
+if __name__ == "__main__" :
+	random = random.Random(43)
+	for _ in range(10) :
+		word = [ chr(random.randrange(ord('a'),ord('z'))) for _ in range(1,MAX_LENGTH)]
+		solve(word)
+		print(word)
