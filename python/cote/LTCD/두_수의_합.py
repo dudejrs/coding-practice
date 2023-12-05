40d32d44b8f3d0f2ade017008ae73a4a1fede95c
kimdeokyoung <dudejrss@naver.com>
2023-12-05 16:55:11 +0900

    feat: 두 수의 합

---
+++ python/cote/LTCD/두_수의_합.py
@@ -0,0 +1,31 @@
+from typing import *
+import random
+
+
+TEST_CASES = 10
+N = 1000
+TARGET = 1.5*N
+
+
+def solve( nums : List[int], target : int) -> List[int] :
+	num_to_index = { num : i for i, num in enumerate(nums)}
+	
+	for i, num in enumerate(nums) : 
+		if target - num in num_to_index and  i != num_to_index[target - num] : 
+			return [i, num_to_index[target - num]]
+
+	return []
+
+if __name__ == "__main__" :
+
+	random.seed(43)
+
+	for _ in range(TEST_CASES) : 
+
+		n = random.randrange(N-1)+1;
+		nums = random.sample(range(0,N-1), n);
+		target =random.randrange(1, TARGET-1)+1
+
+		print(solve(nums,target));
+
+	pass
