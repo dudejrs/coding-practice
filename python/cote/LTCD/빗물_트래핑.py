b29acdd431ec7dc0c3745cb767c15b3380c8a018
kimdeokyoung <dudejrss@naver.com>
2023-12-06 16:48:01 +0900

    feat: 세 수의 합

---
+++ python/cote/LTCD/빗물_트래핑.py
@@ -0,0 +1,64 @@
+from typing import *
+import random
+
+TEST_CASES = 10
+N = 10
+MAX_HEIGHT = 100
+
+
+# 2 Pointer 
+
+def solve1(heights : List[int]) -> int :
+
+	left, right = 0, len(heights)-1
+	left_max, right_max = 0, 0
+	volume = 0
+
+
+	while left < right :
+		left_max, right_max = max(left_max, heights[left]), max(right_max, heights[right])
+
+		if left_max <= right_max :
+			volume += left_max - heights[left]
+			left += 1
+		else :
+			volume += right_max - heights[right]
+			right -= 1
+
+	return volume
+
+
+# Stack
+def solve2(heights : List[int]) -> int :
+
+	stack = []
+	volume = 0
+
+	for i in range(len(heights)) :
+		while stack and heights[i] > heights[stack[-1]] : 
+			top = stack.pop()
+
+			if not len(stack) : 
+				break
+
+			distance = i - stack[-1] -1
+			waters = min(heights[i], heights[stack[-1]]) - heights[top]
+
+			volume += distance * waters
+
+		stack.append(i)
+
+	return volume
+
+
+if __name__ == "__main__" :
+	random.seed(17)
+
+	for _ in range(TEST_CASES) :
+
+		n = random.randrange(2,N) + 2
+		h = random.randrange(1,MAX_HEIGHT) +1
+		heights = [ random.randrange(h) for _ in range(n)]
+		print(solve1(heights), solve2(heights))
+
+
---
+++ python/cote/LTCD/세_수의_합.py
@@ -0,0 +1,56 @@
+from typing import *
+import random
+
+TEST_CASES = 10
+N = 10000
+
+def search(target : int, nums : List[int])-> List[List[int]] :
+	ret = []
+
+	left, right = target+1, len(nums)-1
+
+	while left < right :
+		sum = nums[target] + nums[left] + nums[right] 
+
+		if sum < 0 :
+			left += 1
+		elif sum > 0 :
+			right -= 1
+		else :
+			ret.append(sorted([nums[left], nums[right], nums[target]]))
+
+			while left < right and nums[left] == nums[left+1] :
+				left += 1
+			while left < right and nums[right] == nums[right-1] :
+				right -= 1
+
+			left += 1
+			right -= 1
+
+	return ret
+
+
+def solve(nums : List[int]) -> List[List[int]] : 
+	ret = []
+
+	nums.sort()
+
+	for i in range(len(nums)-2) : 
+		
+		if nums[i] > 0 and nums[i] == nums[i-1] :
+			continue;
+		
+		tmp = search(i, nums)
+		
+		if len(tmp) != 0 :
+			ret.extend(tmp)
+
+	return ret
+
+if __name__ == "__main__" :
+	random.seed(43)
+
+	for _ in range(TEST_CASES) :
+		n = random.randrange(100,N)
+		nums = random.sample(range(-(n//2), n//2), n//2)
+		print(solve(nums))
