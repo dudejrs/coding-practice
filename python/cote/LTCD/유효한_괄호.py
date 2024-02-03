203915015d194e0f40d0392b6fb4bd472020e6df
kimdeokyoung <dudejrss@naver.com>
2024-02-04 00:55:10 +0900

    feat: 유요한 괄호

---
+++ python/cote/LTCD/유효한_괄호.py
@@ -0,0 +1,33 @@
+import random
+
+
+TEST_CASES = 30
+N = 4
+Brakets=['[',']','(',')', '{','}']
+
+
+def solve(s: str) ->bool :
+
+	stack = []
+	braketMap = {
+		'(' : ')',
+		'{' : '}',
+		'[' : ']'
+	}
+
+	for char in s :
+		if char in braketMap.keys() :
+			stack.append(braketMap[char])
+		elif not len(stack) or char != stack.pop() :
+			return False
+
+	return len(stack) == 0
+
+
+if __name__ == "__main__" :
+
+	random.seed(41)
+
+	for _ in range(TEST_CASES) :
+		s = "".join( [random.choice(Brakets) for _ in range(N)] )
+		print(s, solve(s))
