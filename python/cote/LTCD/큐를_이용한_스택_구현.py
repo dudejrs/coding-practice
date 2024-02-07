99078b090d162c1b8c0fca43281e622282414c06
kimdeokyoung <dudejrss@naver.com>
2024-02-08 02:10:58 +0900

    feat: 큐를 이용한 스택 구현

---
+++ python/cote/LTCD/큐를_이용한_스택_구현.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+from typing import TypeVar, Generic
+from collections import deque
+import random
+
+T = TypeVar("T")
+
+class Mystack(Generic[T]):
+	def __init__(self) :
+		self.q = deque()
+
+	def push(self, x: T):
+		self.q.append(x)
+
+		for _ in range(len(self.q)-1) :
+			self.q.append(self.q.popleft())
+
+	def pop(self):
+		return self.q.popleft()
+
+	def top(self):
+		return self.q[0]
+
+	def empty(self):
+		return len(self.q) == 0
+
+
+if __name__ == "__main__" :
+
+	stack = Mystack[int]() 
+	ret = []
+
+	for _ in range(10) :
+		t = random.randrange(100)
+		stack.push(t)
+		ret.append(t)
+
+	print(ret[::-1])
+	print([stack.pop() for _ in range(10)])
