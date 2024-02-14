8a8052b4e7c8d6fba75423c3e6daafbaf687983e
kimdeokyoung <dudejrss@naver.com>
2024-02-14 17:07:00 +0900

    feat: 보석과 돌

---
+++ python/cote/LTCD/보석과_돌.py
@@ -0,0 +1,29 @@
+import random
+from typing import List
+from collections import defaultdict
+
+TEST_CASES = 10
+JEWALS = ord('z')-ord('a')+1
+STONES = 100
+
+
+def solve(jewals: List[int], stones: List[int]) -> int :
+	freq = defaultdict(int)
+
+	for stone in stones :
+		freq[stone] += 1
+	
+	ret = 0
+	for jewal in jewals :
+		ret += freq[jewal]
+		
+	return ret
+
+if __name__ == "__main__" :
+	
+	for _ in range(TEST_CASES) :
+
+		jewals = random.sample([chr(ord('a') + i) for i in range(JEWALS)], random.randrange(1,JEWALS))
+		stones = [ random.choice( [chr(ord('a') + j) for j in range(JEWALS)]) for i in range(STONES) ]
+		print(solve(jewals, stones))
+
