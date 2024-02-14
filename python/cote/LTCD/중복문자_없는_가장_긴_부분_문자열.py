d81cab95e51e9fa3a6b61c51b0c9ee111372d771
kimdeokyoung <dudejrss@naver.com>
2024-02-14 17:28:52 +0900

    feat: 중복문자 없는 가장 긴 부분 문자열

---
+++ python/cote/LTCD/중복문자_없는_가장_긴_부분_문자열.py
@@ -0,0 +1,24 @@
+import random 
+
+TEST_CASES = 10
+N = 10000
+
+def solve(s: str) -> int : 
+	used = {}
+	max_length = start = 0
+	for i, c in enumerate(s) :
+		if c in used and start <= used[c] :
+			start = used[c] + 1
+		else :
+			max_length = max(max_length, i - start + 1)
+		used[c] = i
+
+	return max_length
+
+if __name__ == "__main__" :
+	random.seed(43)
+	for _ in range(TEST_CASES) :
+		l = random.randrange(1,N)
+		s = "".join([chr(random.randrange(ord('a'),ord('z')+1)) for _ in range(l)])
+		print(solve(s))
+
