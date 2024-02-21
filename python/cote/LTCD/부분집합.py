5aaa7cacdcfbd16f12ce1b4fc677e652af8d53e5
kimdeokyoung <dudejrss@naver.com>
2024-02-21 18:05:44 +0900

    feat: 부분집합

---
+++ python/cote/LTCD/부분집합.py
@@ -0,0 +1,26 @@
+from typing import List
+import random
+
+TEST_CASES = 10
+MAX_VALUE = 100
+N = 15
+
+def solve(nums: List[int]) -> List[List[int]] :
+	ret = []
+
+	def dfs(i: int, path: List[int]) :
+		ret.append(path)
+
+		for i in range(i, len(nums)) :
+			dfs(i + 1, [*path, nums[i]])
+
+	dfs(0, [])
+
+	return ret
+
+if __name__ == "__main__" :
+
+	for _ in range(TEST_CASES) :
+		n = random.randrange(1,N)
+		nums = random.sample(range(1,MAX_VALUE), n)
+		print(solve(nums))
