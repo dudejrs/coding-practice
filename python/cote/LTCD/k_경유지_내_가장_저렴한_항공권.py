2a60d334a2117283a444b7105cd97c68846293bd
kimdeokyoung <dudejrss@naver.com>
2024-02-24 12:01:17 +0900

    python: k 경유지 내 가장 저렴한 항공권

---
+++ python/cote/LTCD/k_경유지_내_가장_저렴한_항공권.py
@@ -0,0 +1,56 @@
+from typing import List
+from functools import reduce
+from collections import defaultdict, Counter
+import heapq
+import random
+
+N = 20
+MAX_COST = 100
+TEST_CASES = 10
+
+def initialize(n: int) -> List[int] :
+	ret = []
+
+	for i in range(n) :
+		for j in range(i + 1, n) :
+			if random.random() < 0.25 : 
+				continue
+			ret.append([i, j, random.randrange(1, MAX_COST)])
+
+	counter = Counter(reduce(list.__add__, map(lambda l : [l[0], l[1]], ret)))
+	src = counter.most_common(1)[0][0]
+	dst = counter.most_common(2)[1][0]
+
+	return ret, src, dst
+
+
+def solve(edges: List[int], src: int, dst:int, k: int) -> int :
+
+	graph = defaultdict(list)
+
+	for a, b, w in edges :
+		graph[a].append((b,w))
+		graph[b].append((a,w))
+
+	Q = [(0, src, k)]
+
+	while Q :
+		price, node, k = heapq.heappop(Q)
+
+		if node == dst :
+			return price 
+
+		if k >= 0 :
+			for v, w in graph[node] :
+				heapq.heappush(Q, (price + w, v, k -1))
+
+	return -1
+
+if __name__ == "__main__" :
+	random.seed(43)
+
+	for _ in range(TEST_CASES) :
+		n = random.randrange(3, N)
+		k = random.randrange(1,n)
+		edges, src, dst = initialize(n)
+		print(solve(edges, src, dst, k))
