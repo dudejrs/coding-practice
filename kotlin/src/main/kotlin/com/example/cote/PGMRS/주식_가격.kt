19f346dc914563487c41da6976dd54cd6b6fa91f
kimdeokyoung <dudejrss@naver.com>
2024-02-29 11:37:59 +0900

    kotlin: 주식 가격

---
+++ kotlin/src/main/kotlin/com/example/cote/PGMRS/주식_가격.kt
@@ -0,0 +1,45 @@
+package com.example.cote.PGMRS
+
+import java.util.*
+import kotlin.random.*
+
+val TEST_CASES = 10
+val MAX_PRICES_LENGTH = 10000
+val MAX_PRICES_VALUE = 10000
+
+val random = Random(43)
+
+// 가격이 떨어지지 않은 기간을 남은 배열을 반환 
+fun solve(prices: List<Int>): List<Int> {
+	val ret = MutableList<Int>(prices.size) {0}
+	val stack = Stack<Int>()
+
+	for ((i, price) in prices.withIndex()) {
+
+		while (!stack.isEmpty()) {
+			val top = stack.peek()
+			if (prices[top] <= price) {
+				break
+			}
+			ret[top] = i - top
+			stack.pop()
+		}
+		stack.push(i)
+	}
+
+	while (!stack.isEmpty()) {
+		val top = stack.pop()
+		ret[top] = prices.size - 1 - top
+	}
+
+	return ret
+}
+
+fun main(){
+	repeat(TEST_CASES) {
+		val prices = List(random.nextInt(MAX_PRICES_LENGTH)){
+			random.nextInt(MAX_PRICES_VALUE)
+		}
+		println(solve(prices))
+	}
+}
