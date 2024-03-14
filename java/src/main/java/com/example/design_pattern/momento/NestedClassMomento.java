f692c9d42b0cbf34c6cea5b06a7a87a154983894
kimdeokyoung <dudejrss@naver.com>
2024-03-14 19:42:31 +0900

    java: Nested Class Momento

---
+++ java/src/main/java/com/example/design_pattern/momento/NestedClassMomento.java
@@ -0,0 +1,69 @@
+package com.example.design_pattern.momento;
+
+import java.util.*;
+
+public class NestedClassMomento {
+
+	public static class Originator {
+
+		private static record Momento(int value){}
+
+		private int value;
+
+		Originator(int value) {
+			this.value = value;
+		}
+
+		public int getValue(){
+			return this.value;
+		}
+
+		public void setValue(int value) {
+			this.value = value;
+		}
+
+		public Momento save() {
+			return new Momento(value);
+		}
+
+		public void restore(Momento m) {
+			this.value = m.value;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("value : %d", value);
+		}
+	}
+
+	public static class Caretaker {
+		private Originator originator;
+		private Stack<Originator.Momento> history = new Stack<>();
+
+		Caretaker(Originator originator) {
+			this.originator = originator;
+		}
+		public void save() {
+			Originator.Momento m = originator.save();
+			history.push(m);
+		}
+		public void undo() {
+			if (history.isEmpty()){
+				return;
+			}
+			Originator.Momento m = history.pop();
+			originator.restore(m);
+		}
+	}
+
+	public static void main(String... args) {
+		Originator origin = new Originator(0);
+		Caretaker ct = new Caretaker(origin);
+		ct.save();
+		origin.setValue(1);
+		origin.setValue(2);
+		ct.undo();
+
+		System.out.println(origin);
+	}
+}
