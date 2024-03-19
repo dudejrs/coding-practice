78e66ad9c238b8ebb50bbe79b677aa183b289879
kimdeokyoung <dudejrss@naver.com>
2024-03-19 20:59:33 +0900

    java: Hierarchical State

---
+++ java/src/main/java/com/example/design_pattern/state/HierarchicalState.java
@@ -0,0 +1,69 @@
+package com.example.design_pattern.state;
+
+public class HierarchicalState {
+
+	private static class LightSwitch {
+
+		private State state = new OffState();
+
+		public void on() {
+			state.on(this);
+		}
+
+		public void off() {
+			state.off(this);	
+		}
+	}
+
+	private static interface State {
+
+		void on(LightSwitch ls);
+		void off(LightSwitch ls);
+	
+	}
+
+	private static class OnState implements State {
+	
+		OnState(){
+			System.out.println("Light turned on.");	
+		}
+
+		@Override
+		public void on(LightSwitch ls) {
+
+		}
+		@Override
+		public void off(LightSwitch ls) {
+			System.out.println("Switching light off...");
+			ls.state = new OffState();
+		}
+	}
+
+	private static class OffState implements State {
+
+		OffState(){
+			System.out.println("Light turned off.");	
+		}
+
+		@Override
+		public void on(LightSwitch ls) {
+			System.out.println("Switching light on...");
+			ls.state = new OnState();
+		}
+
+		@Override
+		public void off(LightSwitch ls) {
+			
+		}
+	}
+
+
+	public static void main(String... args){
+		LightSwitch ls = new LightSwitch();
+		ls.on();
+		ls.on();
+		ls.off();
+		ls.off();
+		ls.on();
+	}
+}
