b416c69ca1085f63b727b14f72ce6313945332dd
kimdeokyoung <dudejrss@naver.com>
2024-03-25 13:59:31 +0900

    kotlin: 네트워크

---
+++ kotlin/src/main/kotlin/com/example/cote/PGMRS/네트워크.kt
@@ -0,0 +1,67 @@
+package com.example.cote.PGMRS;
+
+import java.io.*
+import java.nio.file.*
+import kotlin.random.*
+
+val TEST_CASES = 10
+val N = 20
+val random = Random(43)
+
+fun dfs(cur: Int, visited: MutableList<Boolean>, computers: List<List<Int>>) {
+	visited[cur] = true
+
+	for ((i, v) in computers[cur].withIndex()) {
+		if (!visited[i] && v == 1) {
+			dfs(i, visited, computers)
+		}
+	}
+}
+
+fun solve(computers: List<List<Int>>): Int {
+	val visited = MutableList(computers.size) {false}
+	var count = 0
+	for (i in 0..computers.size - 1) {
+		if (!visited[i]) {
+			dfs(i, visited, computers)
+			count++
+		}
+	}
+
+	return count 
+}
+
+fun main() {
+
+	val p = Paths.get(System.getProperty("user.dir") + "/data/네트워크.txt")
+	val reader = Files.newBufferedReader(p)
+
+	repeat(reader.readLine().toInt()) {
+		val numComputer = reader.readLine().toInt()
+		val computers = IntRange(1,numComputer)
+			.map{
+				reader.readLine().split(" ")
+				.map(String::toInt)
+			}
+		val answer = reader.readLine().toInt()
+
+		if (answer != solve(computers)) {
+			println("$answer ${solve(computers)}")
+			return
+		}
+	}
+
+	repeat(TEST_CASES) {
+		val computers = List(N) { i ->
+			val ret = MutableList(N) 
+			{
+				j -> if (i == j || random.nextInt(100) > 90) 1 else 0
+
+			}
+			ret
+		}
+
+		println("${solve(computers)}")
+	}
+
+}
