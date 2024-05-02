abd8cb9f8b44c92e0baa908da7a6d809e6833f45
kimdeokyoung <dudejrss@naver.com>
2024-05-02 17:32:35 +0900

    python: 1의 개수

---
+++ python/cote/LTCD/1비트의_개수.py
@@ -0,0 +1,20 @@
+import random
+
+TEST_CASES = 10
+MAX_VAL = 1000
+
+def solve(n: int)-> int :
+
+	count = 0
+
+	while n :
+		n &= n - 1
+		count +=1
+
+	return count
+
+if __name__ == "__main__" :
+	random.seed(43)
+	for _ in range(TEST_CASES) :
+		n = random.randrange(MAX_VAL)
+		print(solve(n))
