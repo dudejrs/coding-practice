6d89610663c39b6899c6706025c6ae2001876cd7
kimdeokyoung <dudejrss@naver.com>
2025-03-15 20:05:09 +0900

    chore : python Black 포매터 적용

--- python/cote/ETC/기분좋은_식물의_수.py
+++ python/cote/ETC/기분좋은_식물의_수.py
@@ -1,4 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import (
+    dataclass,
+)
 import random
 import heapq
 
@@ -6,40 +8,59 @@ TEST_CASES = 10
 MAX_PLANTS_LEN = 300_000
 MAX_WATERED_LEN = 300_000
 
+
 @dataclass
-class Plant :
-	date : int
-	index : int 
+class Plant:
+    date: int
+    index: int
 
-	def __gt__ (self, other) :
-		return self.date > other.date
+    def __gt__(self, other):
+        return self.date > other.date
 
-	def __iter__(self) :
-		return iter((self.date, self.index))
+    def __iter__(self):
+        return iter(
+            (
+                self.date,
+                self.index,
+            )
+        )
 
-def solve(plants, watered) :
-	h = [Plant(v, i) for i, v in enumerate(plants)] 
-	m = {plant.index : plant  for plant in h }
-	heapq.heapify(h)
 
-	ret= []
-	for today, target in zip(range(1,len(watered) + 1),watered) :
-		m[target].date = today + plants[m[target].index]
-		
-		while h and h[0].date < today  :
-			heapq.heappop(h)
+def solve(plants, watered):
+    h = [Plant(v, i) for i, v in enumerate(plants)]
+    m = {plant.index: plant for plant in h}
+    heapq.heapify(h)
 
-		ret.append(len(h))
+    ret = []
+    for today, target in zip(
+        range(
+            1,
+            len(watered) + 1,
+        ),
+        watered,
+    ):
+        m[target].date = today + plants[m[target].index]
 
-	return ret
+        while h and h[0].date < today:
+            heapq.heappop(h)
 
-if __name__ == "__main__" :
-	random.seed(43)
+        ret.append(len(h))
 
-	for _ in range(TEST_CASES) :
-		n = random.randrange(1, MAX_PLANTS_LEN)
-		m = random.randrange(1, MAX_WATERED_LEN)
-		plants = [random.randrange(m) for _ in range(n)] 
-		watered = [random.randrange(n) for _ in range(m)]
+    return ret
 
-		print(solve(plants, watered))
+
+if __name__ == "__main__":
+    random.seed(43)
+
+    for _ in range(TEST_CASES):
+        n = random.randrange(1, MAX_PLANTS_LEN)
+        m = random.randrange(1, MAX_WATERED_LEN)
+        plants = [random.randrange(m) for _ in range(n)]
+        watered = [random.randrange(n) for _ in range(m)]
+
+        print(
+            solve(
+                plants,
+                watered,
+            )
+        )
