5134809173b0a1501f263426f9f44dee0d8b45ea
kimdeokyoung <dudejrss@naver.com>
2025-06-22 13:12:25 +0900

    python: 이진수 덧셈

---
+++ python/cote/LTCD/test/test_이진수_덧셈.py
@@ -0,0 +1,11 @@
+import pytest
+from ..이진수_덧셈 import solve
+
+@pytest.mark.parametrize("a, b, expected", [
+	("11", "1", "100"),
+	("11", "0", "11"),
+	("0", "11", "11"),
+	("0", "0", "0"),
+	])
+def test_solve(a: str, b: str, expected:str) :
+	assert solve(a, b) == expected
