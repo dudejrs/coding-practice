a856977d1403a1c4322e0c7f0b14b0bcdb9d32e9
kimdeokyoung <dudejrss@naver.com>
2025-06-22 13:14:51 +0900

    style: 스타일 적용

--- python/cote/LTCD/test/test_이진수_덧셈.py
+++ python/cote/LTCD/test/test_이진수_덧셈.py
@@ -1,11 +1,15 @@
 import pytest
 from ..이진수_덧셈 import solve
 
-@pytest.mark.parametrize("a, b, expected", [
-	("11", "1", "100"),
-	("11", "0", "11"),
-	("0", "11", "11"),
-	("0", "0", "0"),
-	])
-def test_solve(a: str, b: str, expected:str) :
-	assert solve(a, b) == expected
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        ("11", "1", "100"),
+        ("11", "0", "11"),
+        ("0", "11", "11"),
+        ("0", "0", "0"),
+    ],
+)
+def test_solve(a: str, b: str, expected: str):
+    assert solve(a, b) == expected
--- python/cote/LTCD/이진수_덧셈.py
+++ python/cote/LTCD/이진수_덧셈.py
@@ -1,19 +1,18 @@
+def solve(a: str, b: str) -> str:
+    max_len = max(len(a), len(b))
+    carry = 0
+    result = []
 
-def solve(a: str, b: str) -> str : 
-	max_len = max(len(a), len(b))
-	carry = 0 
-	result = []
+    for i in range(-1, -max_len - 1, -1):
+        element_a = a[i] if abs(i) <= len(a) else 0
+        element_b = b[i] if abs(i) <= len(b) else 0
 
-	for i in range(-1, -max_len - 1, -1) : 
-		element_a = a[i] if abs(i) <= len(a) else 0
-		element_b = b[i] if abs(i) <= len(b) else 0
+        add = int(element_a) + int(element_b) + int(carry)
+        value = add % 2
+        carry = add // 2
+        result.insert(0, str(value))
 
-		add = int(element_a) + int(element_b) + int(carry)
-		value = add % 2 
-		carry = add // 2
-		result.insert(0, str(value))
+    if carry != 0:
+        result.insert(0, str(carry))
 
-	if carry != 0 :
-		result.insert(0, str(carry))
-
-	return "".join(result)
+    return "".join(result)
