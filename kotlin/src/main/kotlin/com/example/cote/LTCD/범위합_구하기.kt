b84ac7660a8d5e43f7247dfe0b077c4206964787
kimdeokyoung <dudejrss@naver.com>
2025-07-05 15:15:47 +0900

    styles: apply styles

--- kotlin/src/main/kotlin/com/example/cote/LTCD/범위합_구하기.kt
+++ kotlin/src/main/kotlin/com/example/cote/LTCD/범위합_구하기.kt
@@ -3,23 +3,32 @@ package com.example.cote.LTCD
 typealias Matrix = Array<Array<Int>>
 
 object 범위합_구하기 {
-	private fun getRangeSum(matrix: Matrix) : Matrix {
-		val result : Matrix = Array(matrix.size + 1) {
-			Array((matrix[0].size + 1)) {0}
-		}
+    private fun getRangeSum(matrix: Matrix): Matrix {
+        val result: Matrix =
+            Array(matrix.size + 1) { Array((matrix[0].size + 1)) { 0 } }
 
-		for (i in 0 until matrix.size) {
-			for (j in 0 until matrix.size) {
-				result[i + 1][j + 1] = result[i + 1][j] + result[i][j + 1] - result[i][j] + matrix[i][j]
-			}
-		}
+        for (i in 0 until matrix.size) {
+            for (j in 0 until matrix.size) {
+                result[i + 1][j + 1] =
+                    result[i + 1][j] + result[i][j + 1] - result[i][j] +
+                        matrix[i][j]
+            }
+        }
 
-		return result
-	}
+        return result
+    }
 
-	public fun solve(matrix: Matrix, row1: Int, col1: Int, row2: Int, col2: Int) : Int  {
-		val rangeSum = getRangeSum(matrix)
+    public fun solve(
+        matrix: Matrix,
+        row1: Int,
+        col1: Int,
+        row2: Int,
+        col2: Int
+    ): Int {
+        val rangeSum = getRangeSum(matrix)
 
-		return rangeSum[row2 + 1][col2 + 1] - rangeSum[row1][col2 + 1] - rangeSum[row2 + 1][col1] + rangeSum[row1][col1]
-	}
+        return rangeSum[row2 + 1][col2 + 1] -
+            rangeSum[row1][col2 + 1] -
+            rangeSum[row2 + 1][col1] + rangeSum[row1][col1]
+    }
 }
--- kotlin/src/test/kotlin/com/example/cote/LTCD/범위합_구하기Test.kt
+++ kotlin/src/test/kotlin/com/example/cote/LTCD/범위합_구하기Test.kt
@@ -1,37 +1,46 @@
-package com.example.cote.LTCD;
+package com.example.cote.LTCD
 
-import com.example.cote.LTCD.범위합_구하기.solve;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.FieldSource;
+import com.example.cote.LTCD.범위합_구하기.solve
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.FieldSource
 
 typealias Matrix = Array<Array<Int>>
 
 object 범위합_구하기Test {
 
-	@JvmStatic
-	val matrix : Matrix = arrayOf(
-			arrayOf(1, 2, 3, 4, 5),
-			arrayOf(6, 7, 8, 9, 10),
-			arrayOf(11, 12, 13, 14, 15),
-			arrayOf(16, 17, 18, 19, 20),
-			arrayOf(21, 22, 23, 24, 25),
-		)
+    @JvmStatic
+    val matrix: Matrix =
+        arrayOf(
+            arrayOf(1, 2, 3, 4, 5),
+            arrayOf(6, 7, 8, 9, 10),
+            arrayOf(11, 12, 13, 14, 15),
+            arrayOf(16, 17, 18, 19, 20),
+            arrayOf(21, 22, 23, 24, 25),
+        )
 
-	@JvmStatic
-	val testCases = listOf(
-			arrayOf(matrix, 0, 0, 4, 4, 325),
-			arrayOf(matrix, 0, 0, 0, 0, 1),
-			arrayOf(matrix, 1, 1, 2, 2, 40),
-		)
+    @JvmStatic
+    val testCases =
+        listOf(
+            arrayOf(matrix, 0, 0, 4, 4, 325),
+            arrayOf(matrix, 0, 0, 0, 0, 1),
+            arrayOf(matrix, 1, 1, 2, 2, 40),
+        )
 
+    @ParameterizedTest(name = "accuracy test : ({1},{2}), ({3},{4})")
+    @FieldSource("testCases")
+    fun test(
+        matrix: Matrix,
+        row1: Int,
+        col1: Int,
+        row2: Int,
+        col2: Int,
+        expected: Int
+    ) {
+        val result = solve(matrix, row1, col1, row2, col2)
 
-	@ParameterizedTest(name="accuracy test : ({1},{2}), ({3},{4})")
-	@FieldSource("testCases")
-	fun test (matrix:  Matrix, row1: Int, col1: Int, row2: Int, col2: Int, expected: Int) {
-		val result = solve(matrix, row1, col1, row2, col2)
-
-		assertEquals(result, expected, "expected: ${expected} actual: ${result}")
-	}
+        assertEquals(
+            result, expected, "expected: ${expected} actual: ${result}")
+    }
 }
